ebdc77c901d68a3198ae70b5f1ce2837b1d4cfde
BuntinMA_Home <BuntinMA@mail.ru>
2022-11-29 23:05:36 +0300

    Решаю задачу 2

--- Task_02.java
+++ Task_02.java
@@ -1,11 +1,35 @@
+// Вывести все простые числа от 1 до 1000 
+
+//import java.time.LocalDateTime;
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Task_02 {
+    
+//    private static final LocalDateTime.now();
+
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) {
-                continue;
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введте число: ");
+        String input_text = iScanner.nextLine();
+        int number = Integer.parseInt(input_text);
+        iScanner.close();
+        
+        //int[] numbers_array = new int [] {1};
+        ArrayList<Integer> numbers_array = new ArrayList<Integer>();
+
+        for (int i = 2; i <= number; i++) {
+            for (int j = 0; j < numbers_array.size(); j++){
+                if (i % numbers_array.get(j) == 0) {
+                    break;
+                }
+                
             }
-            System.out.println(i);
+            numbers_array.add(i);
+            
+            
         }
+        System.out.println(numbers_array);
         
     }
+    
 }
