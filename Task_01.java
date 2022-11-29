412aded9a1490b7add1d9186904a079d4e154921
BuntinMA_Home <BuntinMA@mail.ru>
2022-11-29 22:13:49 +0300

    Task_01 Готово

--- Task_01.java
+++ Task_01.java
@@ -1,16 +1,27 @@
+// Вычислить n-ое треугольного число(сумма чисел от 1 до n)
+// или n! (произведение чисел от 1 до n)
+
 /**
  * program
  */
-public class Task_01 {
-    public static void main(String[] args) {
-        int value = 321;
-        int count = 0;
 
-        while (value != 0) {
-            value -= 1;
+import java.util.Scanner;
+
+ public class Task_01 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введте число: ");
+        String input_text = iScanner.nextLine();
+        int number = Integer.parseInt(input_text);
+        iScanner.close();
+                
+        int count = 0;
+        int sum = 0;
+        while (count <= number) {
+            sum += count;
             count++;
-            System.out.println(value);
+            
         }
-        System.out.println(count);
+        System.out.printf("%s-е треугольное число = %s \n", number, sum);
     }
 }
