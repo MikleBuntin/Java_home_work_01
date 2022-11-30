6f83d24d5b13649406252bf5849add5e2a48ee86
BuntinMA_Home <BuntinMA@mail.ru>
2022-11-30 23:49:17 +0300

    Одноразовый калькулятор готов!

--- Task_03.java
+++ Task_03.java
@@ -12,13 +12,27 @@ public class Task_03 {
         
         System.out.printf("Введте операцию (+, -, *, /): ");
         String input_text_2 = iScanner.nextLine();
-        
 
         System.out.printf("Введте число 2: ");
         String input_text_3 = iScanner.nextLine();
         double number_2 = Double.parseDouble(input_text_3);
         iScanner.close();
         
+        double out_number = 0;
+        switch (input_text_2) {
+            case "+" : out_number = number_1 + number_2;
+            break;
+
+            case "-" : out_number = number_1 - number_2;
+            break;
+
+            case "*" : out_number = number_1 * number_2;
+            break;
+
+            case "/" : out_number = number_1 / number_2;
+            break;
+        }
+        System.out.printf("Out number = %s, \n", out_number);
     }
 
     
